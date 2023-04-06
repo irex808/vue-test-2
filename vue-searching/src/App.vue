10aee3ba51f77976d60f45c469544a9254da3731
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-06 10:46:51 +0200

    Routing

--- vue-searching/src/App.vue
+++ vue-searching/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/projects/SearchBar.vue';
+import ProjectFilters from './components/projects/ProjectFilters.vue';
 </script>
 
 <template>
@@ -8,13 +9,19 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/create">New Project</RouterLink>
       </nav>
-    </div>
+      <div id="divsearch">
+        <SearchBar id="search" />
+      </div>
+      <div>
+        <ProjectFilters />
+      </div>
+      </div>
   </header>
 
   <RouterView />
@@ -23,7 +30,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  max-height: 20vh;
 }
 
 .logo {
@@ -56,26 +63,37 @@ nav a:first-of-type {
   border: 0;
 }
 
+#divsearch {
+  vertical-align: middle;
+  padding-top: 34px;
+}
+
+#search {
+  text-align: center;
+  width: 90px;
+  border: 1px;
+  border-color: black;
+}
+
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    /* padding-right: calc(var(--section-gap) / 2); */
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    margin: 0 0 0 0;
   }
 
   header .wrapper {
     display: flex;
     place-items: flex-start;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
+    text-align: center;
     font-size: 1rem;
 
     padding: 1rem 0;
