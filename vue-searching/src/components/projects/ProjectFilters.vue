508d3b1180a233debdc0b7af73a46dc994a4a975
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-06 10:48:54 +0200

    Filters

---
+++ vue-searching/src/components/projects/ProjectFilters.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useProjectStore } from '../../stores/ProjectStore.js';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+  import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+let store = useProjectStore();
+
+</script>
+<template>
+    <Menu as="div" class="relative inline-block text-left">
+      <div>
+        <MenuButton class="inline-flex w-full justify-center gap-x-0.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Options
+          <ChevronDownIcon class="-mr-1 h-1 w-1 text-gray-400" aria-hidden="true" />
+        </MenuButton>
+      </div>
+  
+      <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+        <MenuItems class="relative right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div class="py-1">
+            <MenuItem v-slot="{ active }" @click="store.sortByName()">
+              <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Sort by name</a>
+            </MenuItem>
+            <br>
+            <MenuItem v-slot="{ active }" @click="store.sortByStars()">
+              <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Sort by stars</a>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </transition>
+    </Menu>
+  </template>
+  
