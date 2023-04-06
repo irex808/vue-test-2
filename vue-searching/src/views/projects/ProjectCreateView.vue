a088f30ecb242f611021e17cc93fac6a05a01b6e
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-06 10:50:04 +0200

    ProjectCreate view

---
+++ vue-searching/src/views/projects/ProjectCreateView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ProjectCreate from '../../components/projects/ProjectCreate.vue';
+</script>
+<template>
+    <div class="projects">
+    <ProjectCreate />
+</div>
+</template>
+<style>
+@media (min-width: 1024px) {
+  .projects {
+    padding-top: 25px;
+    min-height: 100vh;
+    align-items: top;
+    text-align: center;
+    border-left: 1px;
+    border-color: black;
+  }
+}
+</style>
