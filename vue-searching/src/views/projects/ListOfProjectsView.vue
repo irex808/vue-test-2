f65adaddd5585c56691ad2eb1e0084ea67606e45
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-06 10:47:26 +0200

    ListOfProjectsView created

---
+++ vue-searching/src/views/projects/ListOfProjectsView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ListOfProjects from '../../components/projects/ListOfProjects.vue';
+</script>
+<template>
+    <div class="projects">
+    <ListOfProjects />
+</div>
+</template>
+<style>
+@media (min-width: 1024px) {
+  .projects {
+    min-height: 100vh;
+    align-items: top;
+    text-align: center;
+
+  }
+  .projects table {
+    width: 650px;
+  }
+}
+</style>
