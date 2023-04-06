668a0221d0d2acb8899cbaf20b6371e84c11846a
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-06 10:48:38 +0200

    SearchBar created

---
+++ vue-searching/src/components/projects/SearchBar.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { useProjectStore } from '../../stores/ProjectStore.js';
+let store = useProjectStore();
+
+</script>
+<template>
+    <input @input="store.setSearchedProjects($event.target.value);" type="text" placeholder="search" />
+</template>
