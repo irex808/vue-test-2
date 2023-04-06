30e4de4e464d248f687c4dd9cf5282c776e28c8d
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-06 10:49:42 +0200

    Project component

---
+++ vue-searching/src/components/projects/Project.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+let props = defineProps({
+    project:Object
+})
+let starRate = 0;
+onMounted(() => { starRate = props.project.stars});
+
+function rate(){
+    console.log("STARRATECONST"+": "+starRate)
+    if(starRate === props.project.stars){
+        props.project.stars++;
+    } else {
+        props.project.stars--;
+    }
+}
+</script>
+<template>
+        <section>
+            <div>
+                <table>
+                        <tbody> <tr> 
+                            <td> {{ project.name }} </td>
+                             <td> {{ project.description }} </td> 
+                             <td> {{ project.createdAt }} </td> 
+                             <td> <span @click="rate()"> {{ project.stars }} </span> </td>
+                        </tr></tbody>
+                </table>
+        </div>
+    </section>
+</template>
