fdcdc9dda2a8aaa1e2b472eca939da2181d4fb28
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-04-07 10:36:41 +0200

    Filter button css

--- vue-searching/src/components/projects/ProjectFilters.vue
+++ vue-searching/src/components/projects/ProjectFilters.vue
@@ -8,7 +8,7 @@ let store = useProjectStore()
   <Menu as="div" class="relative inline-block text-left text-xs">
     <div>
       <MenuButton
-        class="inline-flex w-16 justify-center rounded-md bg-white px-3 py-2 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        class="inline-flex w-16 justify-center rounded-md bg-white px-3 py-2 pl-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
         Options
         <ChevronDownIcon class="-mr-1 h-0.5 w-0.5 text-gray-400" aria-hidden="true" />
